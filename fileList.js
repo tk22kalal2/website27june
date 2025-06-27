7069725fcb24cbb67de29fa01e22e7c1acf17b5a
hello world <159936375+tk22kalal2@users.noreply.github.com>
2025-06-27 10:43:08 +0530

    Initialize master2

---
+++ fileList.js
@@ -0,0 +1,6 @@
+const filePaths = [
+    "plugins/anatomyp.html","plugins/biochemestryp.html","plugins/physiologyp.html","plugins/pharmacologyp.html","plugins/pathologyp.html",
+    // Add other file paths as needed
+];
+
+module.exports = filePaths;
